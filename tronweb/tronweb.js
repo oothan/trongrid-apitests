98cb9bb15bd0b662b2beaba8dbe57a7fb0c5c4f1
oothan <mgoothan.psnl@gmail.com>
2025-12-20 19:01:57 +0700

    fix: run point still fail

--- tronweb/tronweb.js
+++ tronweb/tronweb.js
@@ -1,14 +1,14 @@
-const tronweb = require('tronweb');
+const TronWeb = require('tronweb');
 
 let fullNodeUrl = process.env.TRONGRID_ENDPOINT;
 let apiKey = process.env.TRONGRID_KEY;
 
-const httpProvider = tronweb.providers.HttpProvider;
+const httpProvider = TronWeb.providers.HttpProvider;
 const tronNode = new httpProvider(fullNodeUrl);
 const etherNode = new httpProvider(fullNodeUrl);
 const eventServer = new httpProvider(fullNodeUrl);
 
-const tronWeb = new tronweb(tronNode, etherNode, eventServer);
+const tronWeb = new TronWeb(tronNode, etherNode, eventServer);
 tronWeb.setHeader({"TRON-PRO-API-KEY": apiKey});
 
 module.exports = tronWeb;
